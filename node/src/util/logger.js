206a13662f4770f6008911d441b64b276731ba6e
LennyDuan <hod8@aber.ac.uk>
2018-03-11 19:03:06 +0000

    lint fix

--- node/src/util/logger.js
+++ node/src/util/logger.js
@@ -1,11 +1,11 @@
-const log = (value) => console.log(`LOG: ${value}`);
+const log = value => console.log(`LOG: ${value}`);
 
-const info = (value) => console.log(`INFO: ${value}`);
+const info = value => console.log(`INFO: ${value}`);
 
-const debug = (value) => console.log(`DEBUG: ${value}`);
+const debug = value => console.log(`DEBUG: ${value}`);
 
 module.exports = {
   log,
   info,
-  debug
-}
+  debug,
+};
