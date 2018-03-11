d2a3278af4499dd823ceaf1a51b9d0dd9e1008a7
LennyDuan <hod8@aber.ac.uk>
2018-03-11 18:53:24 +0000

    logger util

---
+++ node/src/util/logger.js
@@ -0,0 +1,11 @@
+const log = (value) => console.log(`LOG: ${value}`);
+
+const info = (value) => console.log(`INFO: ${value}`);
+
+const debug = (value) => console.log(`DEBUG: ${value}`);
+
+module.exports = {
+  log,
+  info,
+  debug
+}
