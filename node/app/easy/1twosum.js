5bff15b36641c5671bc4465e0e902735344aaf74
LennyDuan <hod8@aber.ac.uk>
2018-03-11 14:48:47 +0000

    setup eslint

--- node/app/easy/1twosum.js
+++ node/app/easy/1twosum.js
@@ -3,18 +3,19 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  var result = [];
-  var diff = [];
+const twoSum = function (nums, target) {
+  const result = [];
+  const diff = [];
   nums.forEach((ele, index) => {
     console.log(`The ${index} element: ${ele}`);
-    if(diff.includes(ele)) {
+    if (diff.includes(ele)) {
       const end = diff[index];
       return [ele, end];
     }
     const diffNum = target - ele;
     diff.push(diffNum);
-  })
+  });
+
   return null;
 };
 
