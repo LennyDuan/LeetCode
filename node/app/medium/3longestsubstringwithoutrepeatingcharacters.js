652e272b62e643c0c52e7cf782a89c2a3708eed3
LennyDuan <hod8@aber.ac.uk>
2018-03-15 21:24:29 +0000

    init thought

--- node/app/medium/3longestsubstringwithoutrepeatingcharacters.js
+++ node/app/medium/3longestsubstringwithoutrepeatingcharacters.js
@@ -4,6 +4,9 @@ import { info } from '../../src/util/logger';
  * @param {string} s
  * @return {number}
  */
+
+ // Inital thought is using a Queue + hash. Find hash > 1; shift queue util the same.
+ // A max to record the max
 var lengthOfLongestSubstring = function(s) {
 
 };
