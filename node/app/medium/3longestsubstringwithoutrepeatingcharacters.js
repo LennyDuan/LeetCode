9b09066ce49c714b1152083db3cf6e83f57f86fb
LennyDuan <hod8@aber.ac.uk>
2018-03-15 21:14:41 +0000

    init 3

---
+++ node/app/medium/3longestsubstringwithoutrepeatingcharacters.js
@@ -0,0 +1,9 @@
+import { info } from '../../src/util/logger';
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+
+};
---
+++ node/run/medium/3longestsubstringwithoutrepeatingcharacters_test.js
@@ -0,0 +1,29 @@
+import { lengthOfLongestSubstring } from '../../app/medium/3longestsubstringwithoutrepeatingcharacters';
+import { assert } from 'chai';
+
+describe('medium', () => {
+  describe('longestsubstringwithoutrepeatingcharacters', () => {
+    describe('lengthOfLongestSubstring', () => {
+      it('should complete return abc = 3', async () => {
+        const expect = 3
+        const given = 'abcabcbb';
+        const actual = lengthOfLongestSubstring(given);
+        assert.deepEqual(expect, actual);
+      });
+
+      it('should complete return b = 1', async () => {
+        const expect = 3
+        const given = 'bbbbb';
+        const actual = lengthOfLongestSubstring(given);
+        assert.deepEqual(expect, actual);
+      });
+
+      it('should complete return wke = 3', async () => {
+        const expect = 3
+        const given = 'pwwkew';
+        const actual = lengthOfLongestSubstring(given);
+        assert.deepEqual(expect, actual);
+      });
+    });
+  });
+});
