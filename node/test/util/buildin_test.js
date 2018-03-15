280f9e83e53b181528812187ae64edf279f24162
LennyDuan <hod8@aber.ac.uk>
2018-03-15 21:32:17 +0000

    test array as queue

--- node/test/util/buildin_test.js
+++ node/test/util/buildin_test.js
@@ -11,6 +11,44 @@ describe('util', () => {
         stack.pop();
         assert.deepEqual([1, 2, 3], stack);
       });
+
+      it('Should behavior as stack to pop top first', async () => {
+        const stack = [1, 2, 3];
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        assert.deepEqual([1, 2, 3, 4, 5, 6], stack);
+
+        stack.pop();
+        assert.deepEqual([1, 2, 3, 4, 5], stack);
+
+        const top = stack.pop();
+        assert.deepEqual([1, 2, 3, 4], stack);
+        assert.deepEqual(5, top);
+
+      });
+    });
+
+    describe('array as queue', () => {
+      it('Should behavior as queue', () => {
+        const queue = [1, 2, 3];
+        queue.push(4);
+        assert.deepEqual([1, 2, 3, 4], queue);
+
+        queue.shift();
+        assert.deepEqual([2, 3, 4], queue);
+      });
+
+      it('Should behavior as queue to shift first', () => {
+        const queue = [1, 2, 3];
+        queue.push(4);
+        assert.deepEqual([1, 2, 3, 4], queue);
+
+        queue.shift();
+        const first = queue.shift();
+        assert.deepEqual([3, 4], queue);
+        assert.deepEqual(2, first);
+      });
     });
   });
 });
