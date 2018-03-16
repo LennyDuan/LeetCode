a3e1672e93f0139a318cb3f11d4f2426fbb9cdab
LennyDuan <hod8@aber.ac.uk>
2018-03-16 17:42:25 +0000

    init arr

--- node/app/medium/3longestsubstringwithoutrepeatingcharacters.js
+++ node/app/medium/3longestsubstringwithoutrepeatingcharacters.js
@@ -1,4 +1,4 @@
-import { info } from '../../src/util/logger';
+import { info, debug } from '../../src/util/logger';
 
 /**
  * @param {string} s
@@ -8,7 +8,15 @@ import { info } from '../../src/util/logger';
 // Inital thought is using a Queue + hash. Find hash > 1; shift queue util the same.
 // A max to record the max
 const lengthOfLongestSubstring = function (s) {
-  info(s);
+  debug(s);
+  if (s.length == 1) return 1;
+  let max = 0;
+  let map = {};
+  let queue = [];
+  const arr = Array.from(s);
+  for (let i in arr) {
+    info(arr[i]);
+  }
   return s;
 };
 
