2b28a0efe4c0c6fa77310c40b49d88261aeb7792
LennyDuan <hod8@aber.ac.uk>
2018-03-18 16:26:41 +0000

    3 pass basic 4

--- node/run/medium/3longestsubstringwithoutrepeatingcharacters_test.js
+++ node/run/medium/3longestsubstringwithoutrepeatingcharacters_test.js
@@ -18,19 +18,19 @@ describe('medium', () => {
         assert.deepEqual(expect, actual);
       });
 
-      // it('should complete return wke = 3', async () => {
-      //   const expect = 3
-      //   const given = 'pwwkew';
-      //   const actual = lengthOfLongestSubstring(given);
-      //   assert.deepEqual(expect, actual);
-      // });
-      //
-      // it('should complete return tmmzuxt = 5', async () => {
-      //   const expect = 5
-      //   const given = 'tmmzuxt';
-      //   const actual = lengthOfLongestSubstring(given);
-      //   assert.deepEqual(expect, actual);
-      // });
+      it('should complete return wke = 3', async () => {
+        const expect = 3
+        const given = 'pwwkew';
+        const actual = lengthOfLongestSubstring(given);
+        assert.deepEqual(expect, actual);
+      });
+
+      it('should complete return tmmzuxt = 5', async () => {
+        const expect = 5
+        const given = 'tmmzuxt';
+        const actual = lengthOfLongestSubstring(given);
+        assert.deepEqual(expect, actual);
+      });
     });
   });
 });
