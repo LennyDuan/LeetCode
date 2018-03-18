79ac7b3259fe611464ee1a2ab4b6a2f5ccc9d864
LennyDuan <hod8@aber.ac.uk>
2018-03-18 17:14:16 +0000

    80 pass basic

--- node/app/medium/80removeduplicatesfromsortedarray2.js
+++ node/app/medium/80removeduplicatesfromsortedarray2.js
@@ -6,10 +6,11 @@ import { info, debug } from '../../src/util/logger';
  */
 const removeDuplicates = function (nums) {
   const map = {}
-  let length = nums.length;
+  let arr = nums;
+  let length = arr.length;
   let duplicated = 0;
-  for (let i = 0; i < nums.length; i += 1) {
-    const ele = nums[i];
+  for (let i = 0; i < length; i += 1) {
+    const ele = arr[i];
     debug(`Next element: ${ele}`);
 
     if (map[ele] === 1 || map[ele] === 2) {
@@ -21,12 +22,13 @@ const removeDuplicates = function (nums) {
     if (map[ele] > 2) {
       info(`nums of ${ele} is : ${map[ele]}`)
       duplicated =+ 1;
-      nums.splice(i, 1);
+      arr.splice(i, 1);
       map[ele] -= 1;
+      i -= 1;
     }
   }
 
-  return length - duplicated;
+  return arr;
 };
 
 module.exports = {
--- node/run/medium/80removeduplicatesfromsortedarray2_test.js
+++ node/run/medium/80removeduplicatesfromsortedarray2_test.js
@@ -5,15 +5,22 @@ describe('medium', () => {
   describe('80removeduplicatesfromsortedarray2', () => {
     describe('removeDuplicates', () => {
       it('should complete return length = 5', async () => {
-        const expect = 5;
+        const expect = [1, 1, 2, 2, 3]
         const given = [1, 1, 1, 2, 2, 3];
         const actual = removeDuplicates(given);
         assert.deepEqual(expect, actual);
       });
 
       it('should complete return length = 2', async () => {
-        const expect = 2;
+        const expect = [1, 1];
         const given = [1, 1];
+        const actual = removeDuplicates(given);
+        assert.deepEqual(expect, actual);
+      });
+
+      it('should complete return length = 4', async () => {
+        const expect = [1, 1];
+        const given = [1, 1, 1, 1];
         const actual = removeDuplicates(given);
         assert.deepEqual(expect, actual);
       });
