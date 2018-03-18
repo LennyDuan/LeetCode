475ebd4fad05ee4798a430bbf3fa5b4b73821b81
LennyDuan <hod8@aber.ac.uk>
2018-03-18 16:57:09 +0000

    80 happy pass

--- node/app/medium/80removeduplicatesfromsortedarray2.js
+++ node/app/medium/80removeduplicatesfromsortedarray2.js
@@ -4,10 +4,28 @@ import { info, debug } from '../../src/util/logger';
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
+const removeDuplicates = function (nums) {
+  const map = {}
+  for (let i = 0; i < nums.length; i += 1) {
+    const ele = nums[i];
+    debug(`Next element: ${ele}`);
 
+    if (map[ele] === 1 || map[ele] === 2) {
+      map[ele] += 1;
+    } else {
+      map[ele] = 1;
+    }
+
+    if (map[ele] > 2) {
+      info(`nums of ${ele} is : ${map[ele]}`)
+      nums.splice(i, 1);
+      map[ele] -= 1;
+    }
+  }
+
+  return nums;
 };
 
 module.exports = {
   removeDuplicates,
-}
+};
