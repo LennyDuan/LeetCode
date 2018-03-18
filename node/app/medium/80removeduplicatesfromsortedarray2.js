bb84f71f5f9b92912683b5aee7b2617ccbd1c67a
LennyDuan <hod8@aber.ac.uk>
2018-03-18 16:40:30 +0000

    init 80

---
+++ node/app/medium/80removeduplicatesfromsortedarray2.js
@@ -0,0 +1,13 @@
+import { info, debug } from '../../src/util/logger';
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+};
+
+module.exports = {
+  removeDuplicates,
+}
---
+++ node/run/medium/80removeduplicatesfromsortedarray2_test.js
@@ -0,0 +1,15 @@
+import { removeDuplicates } from '../../app/medium/80removeduplicatesfromsortedarray2';
+import { assert } from 'chai';
+
+describe('medium', () => {
+  describe('80removeduplicatesfromsortedarray2', () => {
+    describe('removeDuplicates', () => {
+      it('should complete return length = 5', async () => {
+        const expect = 5;
+        const given = [1, 1, 1, 2, 2, 3];
+        const actual = removeDuplicates(given);
+        assert.deepEqual(expect, actual);
+      });
+    });
+  });
+});
