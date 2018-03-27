02f23cbc76bcc08ac9e4c1c9e482ecb57a66edf0
LennyDuan <hod8@aber.ac.uk>
2018-03-27 13:19:01 +0100

    23 init

--- node/app/hard/23mergeksortedlists.js
+++ node/app/hard/23mergeksortedlists.js
@@ -1,6 +1,16 @@
 import { info, debug } from '../../src/util/logger';
 import { ListNode } from '../../src/util/class/linkedlist';
 
+
+// EASY - Brute Force:  is fucking stupid.
+// Time(O) -> O(NlogN); Space(O) -> O(N)
+
+// Medium - Merge 2 pair:  is not difficult.
+// Time(O) -> O(Nlogk); Space(O) -> O(1)
+
+// A bit Harder - Merge Compare one by one: I used here
+// Time(O) -> O(kN); Space(O) -> O(N)
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -13,8 +23,13 @@ import { ListNode } from '../../src/util/class/linkedlist';
  * @return {ListNode}
  */
 const mergeKLists = function (lists) {
+  info(`${lists.length} ListNodes in this list`);
+  lists.forEach((ele, index) =>
+    debug(`List [${index}]: ${ele.toString()}`));
+  return 0;
 
 };
+
 
 module.exports = {
   mergeKLists,
--- node/run/hard/23mergeksortedlists_test.js
+++ node/run/hard/23mergeksortedlists_test.js
@@ -7,26 +7,20 @@ describe('medium', () => {
     describe('mergeKLists', () => {
       const nodeOne2 = new ListNode(2);
       const nodeOne4 = new ListNode(4);
-      const nodeOne3 = new ListNode(3);
-      nodeOne2.next = nodeOne3;
-      nodeOne3.next = nodeOne4;
+      nodeOne2.next = nodeOne4;
 
-      const nodeTwo5 = new ListNode(5);
+      const nodeTwo3 = new ListNode(5);
       const nodeTwo6 = new ListNode(6);
-      const nodeTwo4 = new ListNode(4);
-      nodeTwo4.next = nodeTwo5;
-      nodeTwo5.next = nodeTwo6;
+      nodeTwo3.next = nodeTwo6;
 
       const nodeThree1 = new ListNode(1);
-      const nodeThree10 = new ListNode(10);
-      const nodeThree8 = new ListNode(8);
-      nodeThree1.next = nodeThree8;
-      nodeThree8.next = nodeThree10;
+      const nodeThree5 = new ListNode(5);
+      nodeThree1.next = nodeThree5;
 
       it('should complete return', async () => {
-        const lists = [nodeOne2.nodeTwo4];
+        const lists = [nodeOne2, nodeTwo3];
         const actual = mergeKLists(lists);
-        assert.deepEqual(actual, '');
+        assert.deepEqual(actual.toString(), '2 -> 3 -> 4 -> 6');
       });
     });
   });
