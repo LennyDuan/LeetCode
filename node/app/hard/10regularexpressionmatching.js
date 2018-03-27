35bba8229f2c02be1225bc187cefd30f6b40f8be
LennyDuan <hod8@aber.ac.uk>
2018-03-27 11:10:49 +0100

    move to V3

--- node/app/hard/10regularexpressionmatching.js
+++ node/app/hard/10regularexpressionmatching.js
@@ -5,6 +5,12 @@ import { info, debug } from '../../src/util/logger';
  * @param {string} p
  * @return {boolean}
  */
+// Use Recursion method
+ var isMatch = function(s, p) {
+     if s
+     return true;
+ };
+
 // const isMatchV1 = function (s, p) {
 //   info(`String is [${s}]\nRegExp is: [${p}]`);
 //   if (p === '.*') return true;
@@ -66,79 +72,79 @@ import { info, debug } from '../../src/util/logger';
 //   return true;
 // };
 
-const isMatch = function (s, p) {
-  let str = s;
-  let reg = p;
-  info(`String is [${s}]\nRegExp is: [${p}]`);
-  if (reg === '.*') return true;
-  if (reg.includes('.*')) {
-    const arr = reg.split('.*');
-    debug(`Check includes: [${arr}]`);
-    return arr.every(ele => str.includes(ele));
-  }
-
-  let strS = '';
-  while (str.length > 0) {
-    debug(`\n------------------\n`);
-
-    debug(`String : [${str}]`);
-    debug(`Reg : [${reg}]`);
-    const strF = str.charAt(0);
-    const regF = reg.charAt(0);
-    debug(`String first: [${strF}]`);
-    debug(`Reg first: [${regF}]`);
-
-    if (regF === '.') {
-      info(`. Match [${regF}]`);
-      info(`String remove: [${str.slice(0, 1)}]`);
-      info(`RegExp remove: [${reg.slice(0, 1)}]`)
-      str = str.slice(1);
-      reg = reg.slice(1);
-    } else if (regF === strF) {
-      info(`[${regF}] Match [${strF}]`);
-      info(`String remove: [${str.slice(0, 1)}]`);
-      info(`RegExp remove: [${reg.slice(0, 1)}]`)
-      if(reg.charAt(1) === '*') {
-        strS = str.slice(0, 1);
-        info(`Prev X* is: [${strS}*]`)
-      }
-
-      str = str.slice(1);
-      reg = reg.slice(1);
-    } else if (reg.charAt(1) === '*') {
-      info(`[${strF}] Match 'X*' - 0`);
-      info(`RegExp remove: [${reg.slice(0, 2)}]`)
-      reg = reg.slice(2);
-    } else if (regF === '*') {
-      // 'a*' Match 'aaaaa'
-      info(`${strF}.. Match ${strS}* - Mutil`);
-      while (reg.charAt(0) === '*') {
-        const st = str.charAt(0);
-        if (st === strS) {
-          info(`String remove: [${str.slice(0, 1)}]`);
-          str = str.slice(1);
-        } else {
-          info(`RegExp remove: [${reg.slice(0, 1)}]`);
-          reg = reg.slice(1);
-        }
-      }
-      strS = '';
-    } else {
-      return false;
-    }
-  }
-
-  if (reg.length > 0) {
-    const relen = reg.length;
-    debug(`remain regExp: [${reg}]`);
-    debug(`relen: [${relen}]`);
-    if (relen % 2 !== 0) return false;
-    for (let ri = 1; ri < relen; ri += 2) {
-      if (relen.charAt(ri) !== '*') return false;
-    }
-  }
-  return true;
-};
+// const isMatchV2 = function (s, p) {
+//   let str = s;
+//   let reg = p;
+//   info(`String is [${s}]\nRegExp is: [${p}]`);
+//   if (reg === '.*') return true;
+//   if (reg.includes('.*')) {
+//     const arr = reg.split('.*');
+//     debug(`Check includes: [${arr}]`);
+//     return arr.every(ele => str.includes(ele));
+//   }
+//
+//   let strS = '';
+//   while (str.length > 0) {
+//     debug(`\n------------------\n`);
+//
+//     debug(`String : [${str}]`);
+//     debug(`Reg : [${reg}]`);
+//     const strF = str.charAt(0);
+//     const regF = reg.charAt(0);
+//     debug(`String first: [${strF}]`);
+//     debug(`Reg first: [${regF}]`);
+//
+//     if (regF === '.') {
+//       info(`. Match [${regF}]`);
+//       info(`String remove: [${str.slice(0, 1)}]`);
+//       info(`RegExp remove: [${reg.slice(0, 1)}]`)
+//       str = str.slice(1);
+//       reg = reg.slice(1);
+//     } else if (regF === strF) {
+//       info(`[${regF}] Match [${strF}]`);
+//       info(`String remove: [${str.slice(0, 1)}]`);
+//       info(`RegExp remove: [${reg.slice(0, 1)}]`)
+//       if(reg.charAt(1) === '*') {
+//         strS = str.slice(0, 1);
+//         info(`Prev X* is: [${strS}*]`)
+//       }
+//
+//       str = str.slice(1);
+//       reg = reg.slice(1);
+//     } else if (reg.charAt(1) === '*') {
+//       info(`[${strF}] Match 'X*' - 0`);
+//       info(`RegExp remove: [${reg.slice(0, 2)}]`)
+//       reg = reg.slice(2);
+//     } else if (regF === '*') {
+//       // 'a*' Match 'aaaaa'
+//       info(`${strF}.. Match ${strS}* - Mutil`);
+//       while (reg.charAt(0) === '*') {
+//         const st = str.charAt(0);
+//         if (st === strS) {
+//           info(`String remove: [${str.slice(0, 1)}]`);
+//           str = str.slice(1);
+//         } else {
+//           info(`RegExp remove: [${reg.slice(0, 1)}]`);
+//           reg = reg.slice(1);
+//         }
+//       }
+//       strS = '';
+//     } else {
+//       return false;
+//     }
+//   }
+//
+//   if (reg.length > 0) {
+//     const relen = reg.length;
+//     debug(`remain regExp: [${reg}]`);
+//     debug(`relen: [${relen}]`);
+//     if (relen % 2 !== 0) return false;
+//     for (let ri = 1; ri < relen; ri += 2) {
+//       if (relen.charAt(ri) !== '*') return false;
+//     }
+//   }
+//   return true;
+// };
 
 
 module.exports = {
