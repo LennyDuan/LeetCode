2b04fc53140374a6820985920194c436bcbc9860
LennyDuan <hod8@aber.ac.uk>
2018-06-08 22:18:40 +0100

    Finishi

--- Eclipse/IntegerToRoman/src/NumberToRoman.java
+++ Eclipse/IntegerToRoman/src/NumberToRoman.java
@@ -26,8 +26,8 @@ public class NumberToRoman {
 			String roman = intToRoman(value);
 			System.out.println("The value " + value + " is Roman Numeral: " + roman);
 			
-			// 如果想执行一次程序结束就用break；
-			break;
+			// 如果想执行一次程序结束就用 break；
+			// break;
 		}
 	}
 	
@@ -35,8 +35,23 @@ public class NumberToRoman {
 	    int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
 	    String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 	    
+	    // 开始创建字符
 	    StringBuilder sb = new StringBuilder();
 	    
+	    // 比如输入值为 2018  ''
+	    // 2018 >= 1000 
+	    // 2018 - 1000 = 1018; 'M'
+	    // 1018 - 1000 = 18; 'MM'
+	    // 18 > 900 ? 18 > 500 ? ... 18 > 40? 继续往下比较
+	    // 18 >= 10
+	    // 18 - 10 = 8; 'MMX'
+	    // 继续比较
+	    // 8 >= 5
+	    // 8 - 5 = 3; 'MMXV'
+	    // 继续比较
+	    // 3 >=1 
+	    // 3 - 1 = 2; 'MMXVI'
+	    // 得出结果 2018: 'MMXVIII'
 	    for(int i=0;i<values.length;i++) {
 	        while(num >= values[i]) {
 	            num -= values[i];
